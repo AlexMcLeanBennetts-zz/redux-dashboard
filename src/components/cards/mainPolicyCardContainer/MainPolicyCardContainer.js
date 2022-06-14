443ee8c09c5674c3bae674050b3331015fbde084
Alex McLean <alex.mclean@bennetts.co.uk>
2022-06-14 14:42:05 +0100

    checks if policyData exists before carrying on

--- src/components/cards/mainPolicyCardContainer/MainPolicyCardContainer.js
+++ src/components/cards/mainPolicyCardContainer/MainPolicyCardContainer.js
@@ -8,34 +8,37 @@ import MainPolicyCard from "./MainPolicyCard";
 function MainPolicyCardContainer() {
 
     const { data: policyData } = useGetPolicyDataQuery();
-    const { car, policyDetails } = policyData
+    if (policyData) {
+        let formattedStartDate = moment(policyData.policyDetails.start).format('Do MMM YY');
+        let formattedEndDate = moment(policyData.policyDetails.end).format('Do MMM YY');
 
-    let formattedStartDate = moment(policyDetails.start).format('Do MMM YY');
-    let formattedEndDate = moment(policyDetails.end).format('Do MMM YY');
+        let carMakeAndModelTextFormatted = (policyData.car.make && policyData.car.model) ?
+            `${policyData.car.make} ${policyData.car.model}` :
+            'Error: No car details found';
 
-    let carMakeAndModelTextFormatted = (car.make && car.model) ?
-        `${car.make} ${car.model}` :
-        'Error: No car details found';
+        let NCBTextFormatted = policyData.policyDetails.NCB > 0 ?
+            `${policyData.policyDetails?.NCB} years` :
+            '0';
 
-    let NCBTextFormatted = policyDetails.NCB > 0 ?
-        `${policyDetails?.NCB} years` :
-        '0';
+        let policyNumberFormatted = policyData?.policyNumber ?
+            policyData.policyNumber :
+            'Error: no policy number found';
 
-    let policyNumberFormatted = policyData.policyNumber ?
-        policyData.policyNumber :
-        'Error: no policy number found';
 
-    return (
-        <MainPolicyCard
-            carDetails={car}
-            policyDetails={policyDetails}
-            carMakeAndModelTextFormatted={carMakeAndModelTextFormatted}
-            NCBTextFormatted={NCBTextFormatted}
-            policyNumberFormatted={policyNumberFormatted}
-            formattedStartDate={formattedStartDate}
-            formattedEndDate={formattedEndDate}
-        />
-    )
+
+
+        return (
+            <MainPolicyCard
+                carDetails={policyData.car}
+                policyDetails={policyData.policyDetails}
+                carMakeAndModelTextFormatted={carMakeAndModelTextFormatted}
+                NCBTextFormatted={NCBTextFormatted}
+                policyNumberFormatted={policyNumberFormatted}
+                formattedStartDate={formattedStartDate}
+                formattedEndDate={formattedEndDate}
+            />
+        )
+    }
 }
 
 export default MainPolicyCardContainer
