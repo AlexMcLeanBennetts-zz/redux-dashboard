c159ff7f7737c18dd2ed57749cd67eecc4fcd78b
Alex McLean <alex.mclean@bennetts.co.uk>
2022-06-14 15:14:01 +0100

    Adds setup for react router in tests

--- src/tests/setup/utils/test-utils.js
+++ src/tests/setup/utils/test-utils.js
@@ -3,11 +3,13 @@ import { render as rtlRender } from '@testing-library/react';
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
+import { createMemoryHistory } from 'history';
 
 import { apiSlice } from 'store/api/apiSlice';
 
 function render(
-    ui,
+    component,
     {
         preloadedState,
         store = configureStore({
@@ -18,13 +20,25 @@ function render(
                 getDefaultMiddleware().concat(apiSlice.middleware),
             preloadedState
         }),
+        route = "/",
         ...renderOptions
     } = {}
 ) {
+
+    const history = createMemoryHistory({
+        initialEntries: [route],
+    });
+
     function Wrapper({ children }) {
-        return <Provider store={store}><Router>{children}</Router></Provider>
+        return (
+            <Provider store={store}>
+                <Router history={history}>
+                    {children}
+                </Router>
+            </Provider>
+        )
     }
-    return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+    return rtlRender(component, { wrapper: Wrapper, ...renderOptions })
 }
 
 //re-export everything
