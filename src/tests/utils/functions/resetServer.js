a48847569462da84ca5b236b6ec0deae5426e8f1
Alex McLean <alex.mclean@bennetts.co.uk>
2022-06-15 14:44:21 +0100

    Bug fix: change to ctx to send json

--- src/tests/utils/functions/resetServer.js
+++ src/tests/utils/functions/resetServer.js
@@ -4,7 +4,7 @@ import { rest } from 'msw';
 function resetServer(response) {
     server.resetHandlers(
         rest.get('http://localhost:3030/dashboard', (req, res, ctx) => {
-            return (res(ctx.status(response)))
+            return (res(ctx.json(response)))
         })
     )
 }
