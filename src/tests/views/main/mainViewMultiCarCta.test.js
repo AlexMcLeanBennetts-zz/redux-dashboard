1a774058d30468f2580aeaf0c4a1b22aadecdabe
Alex McLean <alex.mclean@bennetts.co.uk>
2022-06-24 08:56:40 +0100

    tests for multi car CTA

--- src/tests/views/main/mainViewMultiCarCta.test.js
+++ src/tests/views/main/mainViewMultiCarCta.test.js
@@ -2,8 +2,13 @@ import { screen, render } from 'tests/utils/setup/renderWithReduxAndRouter';
 import Main from 'views/Main';
 
 describe('the main view should have a CTA section for multicar policies', () => {
+    beforeEach(() => {
+        render(<Main />)
+    })
+
     it('should have a heading', async () => {
-        const multicarHeading = await screen.findByRole('heading', { name: /multicar/i });
+        let heading = "Save £50 on each additional car with out Multicar deal"
+        const multicarHeading = await screen.findByRole('heading', { name: heading });
 
         expect(multicarHeading).toBeInTheDocument();
     })
